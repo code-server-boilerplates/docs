8fc3607b8146d11b28d7c73248e6d842e9a1824a
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-06-16 22:40:18 +0800

    chore(docs): update Gatsby config and add some content
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,16 @@ module.exports = {
     {
       resolve: "smooth-doc",
       options: {
-        name: "Smooth DOC Starter",
-        description: "Use your own description...",
-        siteUrl: "https://example.com",
+        name: "Code Server Boilerplates Docs",
+        description: "Documentation website for the Code Server Boilerplates project.",
+        siteUrl: "https://csb-docs.community-lores.gq",
+
+        // for the GH icon at the top
+        githubRepositoryURL: "https://github.com/code-server-boilerplates/starter-pack",
+        // for the edit link in docs
+        githubDocRepositoryURL: "https://github.com/code-server-boilerplates/docs",
+        baseDirectory: __dirname,
+
       },
     },
   ],
