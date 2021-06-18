16d2c3a6b90e461888195a22d02e0bb7b498ac07
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-06-18 13:05:51 +0000

    Switch to @rocketseat/gatsby-theme-docs theme BTW

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,25 @@
 module.exports = {
+  siteMetadata: {
+    siteTitle: `Code Server Boilerplates Docs | The Pins Team`,
+    defaultTitle: `Code Server Boilerplates Docs | The Pins Team`,
+    siteTitleShort: `Code Server Boilerplates Docs`,
+    siteDescription: `Official docs for maintaining deploy-code-server-like templates for maintainers and usage for usual users.`,
+    siteUrl: `https://csb-docs.community-lores.gq`, // will be csb-docs.lores.community soon
+    siteAuthor: `The Pins Team and Docs contributors`,
+    //siteImage: `/banner.png`,
+    siteLanguage: `en`,
+    themeColor: `#8257E6`,
+    basePath: `/`,
+  },
   plugins: [
     {
-      resolve: "smooth-doc",
+      resolve: `@rocketseat/gatsby-theme-docs`,
       options: {
-        name: "Code Server Boilerplates Docs",
-        description: "Documentation website for the Code Server Boilerplates project.",
-        siteUrl: "https://csb-docs.community-lores.gq",
-
-        // for the GH icon at the top
-        githubRepositoryURL: "https://github.com/code-server-boilerplates/starter-pack",
-        // for the edit link in docs
-        githubDocRepositoryURL: "https://github.com/code-server-boilerplates/docs",
-        baseDirectory: __dirname,
-
-        // navigation stuff
-        navItems: [{ title: "Get started", url: "/choose-your-path/"}],
-        sections: [ "Starter Pack", "User's Manual", "For Maintainers", "Reference", "Documentation Contributors Zone" ],
+        basePath: `/`,
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        repositoryUrl: `https://github.com/code-server-boilerplates/docs`,
+        //baseDir: `.`,
       },
     },
   ],
