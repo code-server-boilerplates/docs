39d0ad5acb244e843d5608670089fb56950da944
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-06-19 22:20:05 +0800

    Fix build issues BTW
    Also add few more text.

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,8 +13,10 @@ export default function NotFound() {
         If everything is fine and you&#39;d like to go back to homepage, <Link to="/">click here</Link>
         .
       </p>
-      </br>
-      <p>But if you&#39;re in doubt, <Link to="https://github.com/code-server-boilerplates/docs/issues">file an new GitHub issue here</Link>.</p>
+      <p>
+        But if you&#39;re still in doubt, <Link to="https://github.com/code-server-boilerplates/docs/issues">file an new GitHub issue here</Link>
+        so we can fix the problem and redirect you to the right place in the future.
+      </p>
     </Layout>
   );
 }
