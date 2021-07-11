cf4f5f0c191f49990a3890da01b8d7b66d0134a3
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-07-11 11:59:13 +0000

    Update 404 page
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,14 +8,12 @@ export default function NotFound() {
   return (
     <Layout title="Page not found!">
       <SEO title="404: Not found" />
-      <p>You just gone to the wrong route. Did we just broke something?</p>
+      <p>You just gone to the wrong route. Did we just broke something, or the chaos monkeys had taken over the documentation maintainers?</p>
       <p>
-        If everything is fine and you&#39;d like to go back to homepage, <Link to="/">click here</Link>
-        .
+        If everything is fine and you&#39;d like to go back to homepage, <Link to="/">click here</Link>.
       </p>
       <p>
-        But if you&#39;re still in doubt, <Link to="https://github.com/code-server-boilerplates/docs/issues">file an new GitHub issue here</Link>
-        so we can fix the problem and redirect you to the right place in the future.
+        But if you&#39;re still in doubt, <Link to="https://github.com/code-server-boilerplates/docs/issues">file an new GitHub issue here</Link> so we can fix the problem and redirect you to the right place in the future.
       </p>
     </Layout>
   );
